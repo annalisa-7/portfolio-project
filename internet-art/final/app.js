38ece649745cff458bcae04b3a09d93aafab6eec
annalisa-7 <you@example.com>
2023-04-20 12:31:30 -0400

    collision

--- internet-art/final/app.js
+++ internet-art/final/app.js
@@ -1,25 +1,102 @@
+let numBalls = 50;
+let spring = 0.85;
+let gravity = 0;
+let friction = 0;
+let balls = [];
+
 function setup() {
     // noCursor();
     myCanvas = createCanvas(2000,windowHeight);
     myCanvas.parent('myContainer');
-    // createCanvas(720, 400);
-    // hue, saturation, and brightness
-    // colorMode(HSB, 255);
-    // slider has a range between 0 and 255 with a starting value of 127
-    // slider = createSlider(0, 255, 127);
-    fill(0);
-    rect(1500,100,300,463.65);
+    for (let i = 0; i < numBalls; i++) {
+        balls[i] = new Ball(
+          random(width),
+          random(height),
+          random(30, 70),
+          i,
+          balls
+        );
+      }
+      noStroke();
+      fill(255, 204);
 }
 
 function draw() {
-    background(127);
-    strokeWeight(2);
+    background(128);
 
-    fill(0);
+    // fill(0);
     rect(1500,100,300,463.65);
+    rect(20,100,300,400);
+    rect(1000,50,300,614.75);
+    rect(500,200,300,400);
+
+    balls.forEach(ball => {
+        ball.collide();
+        ball.move();
+        ball.display();
+      });
+}
+
+class Ball {
+    constructor(xin, yin, din, idin, oin) {
+      this.x = xin;
+      this.y = yin;
+      this.vx = 0;
+      this.vy = 0;
+      this.diameter = din;
+      this.id = idin;
+      this.others = oin;
+      this.color = color(128 + random(128),128 + random(128),128 + random(128),127);
+    //   this.stroke = color(random(255),255,255);
+    }
   
-    // // Set the hue according to the slider
-    // stroke(slider.value(), 255, 255);
-    // fill(slider.value(), 255, 255, 127);
-    // ellipse(360, 200, 200, 200);
+    collide() {
+      for (let i = this.id + 1; i < numBalls; i++) {
+        // console.log(others[i]);
+        let dx = this.others[i].x - this.x;
+        let dy = this.others[i].y - this.y;
+        let distance = sqrt(dx * dx + dy * dy);
+        let minDist = this.others[i].diameter / 2 + this.diameter / 2;
+        //   console.log(distance);
+        //console.log(minDist);
+        if (distance < minDist) {
+          //console.log("2");
+          let angle = atan2(dy, dx);
+          let targetX = this.x + cos(angle) * minDist;
+          let targetY = this.y + sin(angle) * minDist;
+          let ax = (targetX - this.others[i].x) * spring;
+          let ay = (targetY - this.others[i].y) * spring;
+          this.vx -= ax;
+          this.vy -= ay;
+          this.others[i].vx += ax;
+          this.others[i].vy += ay;
+        }
+      }
+    }
+
+  move() {
+    this.vy += gravity;
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.x + this.diameter / 2 > width) {
+      this.x = width - this.diameter / 2;
+      this.vx *= friction;
+    } else if (this.x - this.diameter / 2 < 0) {
+      this.x = this.diameter / 2;
+      this.vx *= friction;
+    }
+    if (this.y + this.diameter / 2 > height) {
+      this.y = height - this.diameter / 2;
+      this.vy *= friction;
+    } else if (this.y - this.diameter / 2 < 0) {
+      this.y = this.diameter / 2;
+      this.vy *= friction;
+    }
+  }
+
+  display() {
+    fill(this.color);
+    // stroke(this.stroke);
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
 }
