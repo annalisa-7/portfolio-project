d7cadb5410f5ccb242f303842611bc8b9ce523e6
annalisa-7 <you@example.com>
2023-04-23 18:04:33 -0400

    final

--- internet-art/final/app.js
+++ internet-art/final/app.js
@@ -1,8 +1,28 @@
-let numBalls = 50;
-let spring = 0.85;
+let numBalls = 40;
+let spring = 0.5;
 let gravity = 0;
 let friction = 0;
 let balls = [];
+let space = [];
+
+
+// creating array for x-position of balls (so don't get stuck in the rectangles)
+for (let i = 0; i < 20; i++) {
+  space[i] = i;
+}
+for (let j = 320; j < 500; j++) {
+  space[j-300] = j;
+}
+for (let k = 800; k < 1000; k++) {
+  space[k-600] = k;
+}
+for (let l = 1300; l < 1500; l++) {
+  space[l-900] = l;
+}
+
+for (let m = 1800; m < 2000; m++) {
+  space[m-1200] = m;
+}
 
 function setup() {
     // noCursor();
@@ -10,7 +30,7 @@ function setup() {
     myCanvas.parent('myContainer');
     for (let i = 0; i < numBalls; i++) {
         balls[i] = new Ball(
-          random(width),
+          random(space),
           random(height),
           random(30, 70),
           i,
@@ -22,27 +42,32 @@ function setup() {
 }
 
 function draw() {
-    background(128);
+    background(163,130,128);
 
-    // fill(0);
-    rect(1500,100,300,463.65);
-    rect(20,100,300,400);
-    rect(1000,50,300,614.75);
-    rect(500,200,300,400);
+    // rect(5,5,100,100)
 
     balls.forEach(ball => {
         ball.collide();
         ball.move();
         ball.display();
       });
+
+      // rectangles where images are
+      fill(163,130,128);
+      rect(1500,100,300,463.65);
+      rect(20,100,300,400);
+      rect(1000,50,300,614.75);
+      rect(500,200,300,400);
 }
+
+// adapted from: https://p5js.org/examples/motion-bouncy-bubbles.html
 
 class Ball {
     constructor(xin, yin, din, idin, oin) {
       this.x = xin;
       this.y = yin;
-      this.vx = 0;
-      this.vy = 0;
+      this.vx = random(1);
+      this.vy = random(1);
       this.diameter = din;
       this.id = idin;
       this.others = oin;
@@ -72,26 +97,62 @@ class Ball {
           this.others[i].vy += ay;
         }
       }
+
+      //bounce off of walls
+      if (this.x <= (this.diameter/2) || this.x >= width-(this.diameter/2)) {
+        this.vx *= -1;
+      }
+
+      if (this.y <= (this.diameter/2) || this.y >= height-(this.diameter/2)) {
+        this.vy *= -1;
+      }
+
+    //   if (this.x - this.diameter/2 <= 0) {
+    //     this.x = this.diameter/2; 
+    //   } else {
+    //     this.x = width - this.diameter/2; 
+    //   }
+      
+    //   this.resetVelocityAndAcceleration();
+    // }
+
+      //bounce off of images
+      if (this.x + this.diameter/2 > 1500 && this.x - this.diameter/2 < 1800 && this.y + this.diameter/2 > 100 && this.y - this.diameter/2 < 563.65) {
+        this.vx *= -0.7;
+        this.vy *= -0.7;
+      }
+      if (this.x + this.diameter/2 > 20 && this.x - this.diameter/2 < 320 && this.y + this.diameter/2 > 100 && this.y - this.diameter/2 < 500) {
+        this.vx *= -0.7;
+        this.vy *= -0.7;
+      }
+      if (this.x + this.diameter/2 > 1000 && this.x - this.diameter/2 < 1300 && this.y + this.diameter/2 > 50 && this.y - this.diameter/2 < 664.75) {
+        this.vx *= -0.7;
+        this.vy *= -0.7;
+      }
+      if (this.x + this.diameter/2 > 500 && this.x - this.diameter/2 < 800 && this.y + this.diameter/2 > 200 && this.y - this.diameter/2 < 600) {
+        this.vx *= -0.7;
+        this.vy *= -0.7;
+      }
     }
 
   move() {
     this.vy += gravity;
     this.x += this.vx;
     this.y += this.vy;
-    if (this.x + this.diameter / 2 > width) {
-      this.x = width - this.diameter / 2;
-      this.vx *= friction;
-    } else if (this.x - this.diameter / 2 < 0) {
-      this.x = this.diameter / 2;
-      this.vx *= friction;
-    }
-    if (this.y + this.diameter / 2 > height) {
-      this.y = height - this.diameter / 2;
-      this.vy *= friction;
-    } else if (this.y - this.diameter / 2 < 0) {
-      this.y = this.diameter / 2;
-      this.vy *= friction;
-    }
+    // if (this.x + this.diameter / 2 > width) {
+    //   this.x = width - this.diameter / 2;
+    //   this.vx *= friction;
+    // } else if (this.x - this.diameter / 2 < 0) {
+    //   this.x = this.diameter / 2;
+    //   this.vx *= friction;
+    // }
+    // if (this.y + this.diameter / 2 > height) {
+    //   this.y = height - this.diameter / 2;
+    //   this.vy *= friction;
+    // } else if (this.y - this.diameter / 2 < 0) {
+    //   this.y = this.diameter / 2;
+    //   this.vy *= friction;
+    // }
   }
 
   display() {
